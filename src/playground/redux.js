24ff1b9d876725aa1783c7795c8259c7cfde66dd
zerogpm <zerogpm@gmail.com>
2018-08-29 20:30:16 -0700

    Refactoring and organizing

--- src/playground/redux.js
+++ src/playground/redux.js
@@ -1,26 +1,42 @@
 import { createStore } from 'redux';
 
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const restCount = () => ({
+  type: 'RESET'
+});
+
+const setCount = ({ setCountBy = 101 } = {}) => ({
+  type: 'SET',
+  setCountBy
+});
+
 const store = createStore((state = {count: 0}, action) => {
 
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'RESET':
       return {
         count: 0
       };
     case 'SET':
-      const defaultValue = typeof action.count === 'number' ? action.count : 1;
       return {
-        count: defaultValue
+        count : action.setCountBy
       };
     default:
       return state;
@@ -31,29 +47,14 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({incrementBy: 5}));
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(restCount());
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({ setCountBy: 1000 }));
