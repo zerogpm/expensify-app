c88423613fa0b878af846113d40f28ba268752d8
zerogpm <zerogpm@gmail.com>
2018-08-29 17:25:27 -0700

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,19 +1,19 @@
-// const person = {
-//   name: 'chris',
-//   age: 26,
-//   location: {
-//     city: 'shenzhen',
-//     temp: 92
-//   }
-// };
-//
-// const {name = 'people', age} = person;
-//
-// console.log(`${name} is ${age}`);
-//
-// const {temp:temper, city} = person.location;
-//
-// console.log(`its ${temper} in ${city}`);
+const person = {
+  name: 'chris',
+  age: 26,
+  location: {
+    city: 'shenzhen',
+    temp: 92
+  }
+};
+
+const {name = 'people', age} = person;
+
+console.log(`${name} is ${age}`);
+
+const {temp:temper, city} = person.location;
+
+console.log(`its ${temper} in ${city}`);
 
 const book = {
   title: 'Ego is the Enemy',
@@ -25,4 +25,13 @@ const book = {
 
 const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+
+const address = ['1299 S shenzhen', 'shenzhen', 'guangdong', '11122'];
+
+const [street, city, , zip] = address;
+
+console.log(`You are in ${street} ${city} ${zip}`);
+
+const item = ['Coffee(hot)', '$2.00', '$2.50', '$2.75'];
+const [coffee, , price, ] = item;
+console.log(`A medium ${coffee} costs ${price}`);
