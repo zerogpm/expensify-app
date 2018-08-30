43b051b45069056483cc555e9577539ae70f21c2
zerogpm <zerogpm@gmail.com>
2018-08-30 09:45:17 -0700

    spread operator in reducers

--- src/playground/redux-exp.js
+++ src/playground/redux-exp.js
@@ -1,4 +1,82 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//Add Expense
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+//remove Expense
+const removeExpense = (
+  {
+    id = ''
+  } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  expense: {
+    id
+  }
+});
+
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date', //date or amount
+  startDate: undefined,
+  endDate: undefined
+};
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(item => item.id !== action.expense.id);
+    default:
+      return state;
+  }
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300}));
+
+store.dispatch(removeExpense({
+  id: expenseOne.expense.id
+}));
+
+console.log('id to remove', expenseOne.expense.id);
 
 const demoState = {
   expenses: [{
