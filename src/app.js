2a328bf011882d65a3b4ad9ce9843de4b8b675b9
zerogpm <zerogpm@gmail.com>
2018-08-31 21:49:29 -0700

    Dropdown for picking sortby

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -12,12 +11,9 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt:-21000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt:-1000}));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Rent bill', amount: 200, createdAt:-21000}));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 50, createdAt:-1000}));
 
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-},3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
 
 const ExpenseListFilters = (props) => (
   <div>
     <input type="text" value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value));
     }}/>
+    <select
+      onChange={(e) => {
+      if (e.target.value === 'date') {
+        props.dispatch(sortByDate());
+      }
+
+      if (e.target.value === 'amount') {
+        props.dispatch(sortByAmount());
+      }
+    }}
+      value={props.filters.sortBy}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
